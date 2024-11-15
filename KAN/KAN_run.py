12602167fa6bf5f80dc1ff3bb61e2109db5b9f7c
antonschulz <anton.ph.schulz@gmail.com>
2024-11-15 14:11:37 +0100

    structured things

--- KAN/KAN_run.py
+++ KAN/KAN_run.py
@@ -18,20 +18,26 @@ torch.manual_seed(0)
 def read_data(filepath:str) -> pd.DataFrame:
     return pd.read_csv(filepath)
 
-def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict:
-    # Initialize model and create dataset
+def basic_fit(folder_name : str, cfg : dict) -> dict:
+    # create dataset
+    train_data = read_data(f"./datasets/{folder_name}/train_data.csv")
+    val_data = read_data(f"./datasets/{folder_name}/validation_data.csv")
+    test_data = read_data(f"./datasets/{folder_name}/test_data.csv")
+    total_data = read_data(f"./datasets/{folder_name}/true_data.csv")
+    ## read params
+    layers = cfg["layers"]
+    grid = cfg["grid"]  # nr of spline grids
+    k = cfg["k"] #order of spline
+    seed = cfg.get("seed", 0) #random seed
+    opt = cfg.get("opt", "Adam") #optimizer
+    steps = cfg.get("steps", 800) #nr of steps
+    lr = cfg.get("lr", 1e-3)#learning rate
+    lamb = cfg.get("lamb", 0.0) #regularization parameter
+    ## ----------------------------------------
+    results_folder_name = f'{folder_name}_{layers}_241114' #save folder name, one for each model run
+    os.makedirs(f'./KAN/results/{results_folder_name}', exist_ok=True)
 
-    ## Params ## TODO: Choose architecture + choose save folder name
-    width = [1, 7, 7, 7, 1] #width for each layer
-    grid = 3  # nr of spline grids
-    k = 3 #order of spline
-    seed = 0 #random seed
-    folder_name = f'uniform_sin(x)_{width}_241114' #save folder name, one for each model run
-    os.makedirs(f'./KAN/results/{folder_name}', exist_ok=True)
-
-    kan_model = KAN(width=width, grid=grid, k=k, seed=seed)
-
-
+    # Prepare data
     X_tot = torch.tensor(total_data['x']).float().unsqueeze(1)
     #y_noise_tot = torch.tensor(total_data['y_noise']).float()#.unsqueeze(1)
     y_true_tot = torch.tensor(total_data['y_true']).float().unsqueeze(1)
@@ -48,20 +54,13 @@ def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict
     #y_noise_test = torch.tensor(test_data['y_noise']).float().unsqueeze(1)
     #y_true_test = torch.tensor(test_data['y_true']).float().unsqueeze(1)
 
-
-
     dataset = {"train_input": y_noise_train, "train_label":y_true_train, "test_input":y_noise_val, "test_label":y_true_val}
 
 
     # Train model
-    ## Params ## TODO: can change params, only steps for friday 14 nov
     dataset_input = "y_noise, y_true"
-    opt = "Adam"
-    steps = 800
-    lr = 0.01
-    lamb = 0.0
     
-
+    kan_model = KAN(width=layers, grid=grid, k=k, seed=seed)
     start = time.time()
     results = kan_model.fit(dataset, opt=opt, steps=steps, lr=lr , lamb=lamb)
 
@@ -87,12 +86,12 @@ def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict
     if len(results['train_loss']) != len(results['test_loss']):
         raise ValueError("'train_loss' and 'test_loss' must be of the same length.")
 
+    """
+    Plot the data, true function, and KAN predictions.
+    """
     # Set Seaborn theme
     sns.set_theme(style="whitegrid")
-
-    # Create a figure with two subplots side by side
-    #fig, ax = plt.subplots(1, 2, figsize=(16, 6))
-
+    
     # --- Plot 1: Data and Predictions ---
     print("Test Input Shape:", dataset['test_input'].shape)
     print("Test Label Shape:", dataset['test_label'].shape)
@@ -110,11 +109,14 @@ def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict
     plt.title("Prediction using KAN", fontsize=14, weight='bold')
 
     plt.tight_layout()
-    plt.savefig(f'./KAN/results/{folder_name}/plot.png', dpi=300)
+    plt.savefig(f'./KAN/results/{results_folder_name}/plot.png', dpi=300)
 
     plt.clf()
 
-    # --- Plot 2: Training and Validation Loss ---
+    """
+    
+    Plot the training and validation loss over epochs.
+    """
     # Convert loss data to a DataFrame for Seaborn
     loss_df = pd.DataFrame({
         'Epoch': range(1, len(results['train_loss']) + 1),
@@ -148,14 +150,7 @@ def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict
     # Adjust layout for better spacing
     plt.tight_layout()
 
-    
-
-    plt.savefig(f'./KAN/results/{folder_name}/loss.png', dpi=300)
-    # Optional: Save the figure with high resolution 
-    
-
-    # Display the plots
-    #plt.show()
+    plt.savefig(f'./KAN/results/{results_folder_name}/loss.png', dpi=300)
 
     print(f"Elapsed Time: {elapsed_time:.3f} seconds")
 
@@ -163,11 +158,12 @@ def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict
 
     
     
-    file_path = f'./KAN/results/{folder_name}/model_params.txt'
+    file_path = f'./KAN/results/{results_folder_name}/model_params.txt'
 
     with open(file_path, "w") as file:
         file.write(f"input to model: {dataset_input}\n")
-        file.write(f"width: {width}\n")
+        # write cfg
+        file.write(f"layers: {layers}\n")
         file.write(f"grid: {grid}\n")
         file.write(f"k: {k}\n")
         file.write(f"seed: {seed}\n")
@@ -178,81 +174,28 @@ def basic_fit(train_data: pd.DataFrame, val_data, test_data, total_data) -> dict
         file.write(f"final validation loss: {loss_df['Validation Loss'].iloc[-1]}\n")
         file.write(f"final training loss: {loss_df['Train Loss'].iloc[-1]}\n")
             
-    
-    np.savez(f'./KAN/results/{folder_name}/plots.npz', epoch=range(1, len(results['train_loss']) + 1), train_loss = results['train_loss'], val_loss = results['test_loss'] , X_val=X_val, y_noise_val=y_noise_val , X_tot = X_tot, y_true_tot = y_true_tot, sorted_X = sorted_X, sorted_KAN_preds = sorted_KAN_preds)
+    print(f"Model parameters saved to {file_path}")
+    np.savez(f'./KAN/results/{results_folder_name}/plots.npz', epoch=range(1, len(results['train_loss']) + 1), train_loss = results['train_loss'], val_loss = results['test_loss'] , X_val=X_val, y_noise_val=y_noise_val , X_tot = X_tot, y_true_tot = y_true_tot, sorted_X = sorted_X, sorted_KAN_preds = sorted_KAN_preds)
         
     return results
 
-#### Main():
 
-### Import dataset
-import_data_folder = "uniform_sin(x)_241114"  ## TODO: select dataset
+def main():
+    # TODO: Implement cross-validation and grid search for hyperparameter tuning
+    # TODO: Implement additional hyperparameter configurations
+    default = "uniform_sin(x)_241114"
+    configs = [{
+        "folder_name": default,
+        "layers" : [1, 3, 3, 1],
+        "grid" : 3,
+        "k" : 3,
+        "lr": 0.01, 
+        "steps": 800,
+        "lamb": 0.0,
+    }]
+    for cfg in configs:
+        print(f"Running model for {cfg['folder_name']}")
+        basic_fit(cfg["folder_name"], cfg)
 
-train_data = read_data(f"./datasets/{import_data_folder}/train_data.csv")
-val_data = read_data(f"./datasets/{import_data_folder}/validation_data.csv")
-test_data = read_data(f"./datasets/{import_data_folder}/test_data.csv")
-total_data = read_data(f"./datasets/{import_data_folder}/true_data.csv")
-
-### Train model, prediction and plot
-basic_fit(train_data=train_data, val_data=val_data, test_data=test_data, total_data=total_data)
-
-
-### Plot from data
-"""
-data = np.load("./KAN/results/result3/plots.npz", allow_pickle=True)
-print(data["X_train"].shape)
-print(data["y_noise_train"].shape)
-print(data["sorted_KAN_preds"].shape)
-print(data["epoch"].shape)
-print(data["train_loss"].shape)
-print(data["val_loss"].shape)
-
-
-
-loss_df = pd.DataFrame({
-        'Epoch': data["epoch"],
-        'Train Loss': data["train_loss"],
-        'Validation Loss': data["val_loss"]})
-
-loss_melted = loss_df.melt(id_vars='Epoch', var_name='Loss Type', value_name='Loss')
-
-sns.set_theme(style="whitegrid")
-
-fig, ax = plt.subplots(1, 2, figsize=(16, 6))
-
-ax[0].plot(data["X_train"], data["y_noise_train"], marker='o', markersize=1, linestyle='None', label="train data")
-ax[0].plot(data["X_tot"], data["y_true_tot"], linestyle='-',label='True function')
-
-sorted_X = data["sorted_X"]
-sorted_KAN_preds = data["sorted_KAN_preds"]
-
-
-#print("sorted_KAN_preds",sorted_KAN_preds.shape)
-ax[0].plot(sorted_X, sorted_KAN_preds, "--", label='KAN predictions')
-
-ax[0].set_xlabel("Random X 1D samples")
-ax[0].set_ylabel("Function")
-ax[0].legend()
-
-
-
-sns.lineplot(data=loss_melted, x='Epoch', y='Loss', hue='Loss Type',
-                 ax=ax[1], marker='.')
-
-# Set labels and title
-ax[1].set_xlabel("Epoch", fontsize=12)
-ax[1].set_ylabel("Loss", fontsize=12)
-ax[1].set_title("Training and Validation Loss Over Epochs", fontsize=14, weight='bold')
-
-# Customize legend
-ax[1].legend(title='Loss Type', fontsize=10, title_fontsize=12)
-
-# Adjust layout for better spacing
-plt.tight_layout()
-
-# Optional: Save the figure with high resolution
-# plt.savefig('enhanced_plots.png', dpi=300)
-
-# Display the plots
-plt.show()
-"""
+if __name__ == "__main__":
+    main()
