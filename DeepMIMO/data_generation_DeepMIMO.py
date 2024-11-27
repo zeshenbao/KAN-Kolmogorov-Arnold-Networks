ce49800402f44798526f841bfd9eac4bf013a52f
madeli <madeli@kth.se>
2024-11-27 11:07:34 +0100

    Generating H matrix

--- DeepMIMO/data_generation_DeepMIMO.py
+++ DeepMIMO/data_generation_DeepMIMO.py
@@ -13,8 +13,8 @@ def create_dataset(scenario_name,
     # Which scenario
     params['scenario'] = scenario_name
     # For the antennas
-    params['bs_antenna']['shape'] = np.array([n_bs_y, n_bs_x])
-    params['ue_antenna']['shape'] = np.array([n_ue_y, n_ue_x])
+    #params['bs_antenna']['shape'] = np.array([n_bs_y, n_bs_x])
+    #params['ue_antenna']['shape'] = np.array([n_ue_y, n_ue_x])
     # For the path
     params['num_paths'] = n_paths
     params['OFDM']['subcarriers'] = n_subcarriers
@@ -25,9 +25,10 @@ def create_dataset(scenario_name,
     return dataset
 
 def generate_H(dataset):
-    i = 0
-    j = 0
+    i = 0  # Only one base station in scenario I2
+    j = 0  # Only the first user
     H = dataset[i]['user']['channel'][j]
+    H = H[:, :, 0]  # Choose first subcarrier
     return H
 
 def generate_H_abs(dataset):
@@ -36,15 +37,22 @@ def generate_H_abs(dataset):
 
     return H_abs
 
+def generate_noisy_H(H):
+    noise = np.random.normal(0, 10**(-6), H.shape)
+    H_noise = H + noise
+
+    return H_noise
+
+
 def plot_heatmat(H):
     plt.figure(figsize=(8, 6))
-    plt.imshow(H, cmap='viridis', aspect='auto', origin='lower', interpolation='nearest')
+    plt.imshow(H.T, cmap='viridis', aspect='auto', origin='lower', interpolation='nearest')
     plt.colorbar(label='Channel Gain Magnitude')
-    plt.title('Channel Gain Magnitude of (BS, UE) Antenna Pairs')
-    plt.xlabel('BS Index')
-    plt.ylabel('UE Index')
-    plt.xticks(ticks=np.arange(H.shape[1]), labels=np.arange(1, H.shape[1] + 1))
-    plt.yticks(ticks=np.arange(H.shape[0]), labels=np.arange(1, H.shape[0] + 1))
+    plt.title('Channel Gain Magnitude of (RX, TX) Antenna Pairs')
+    plt.xlabel('RX antenna')
+    plt.ylabel('TX antenna')
+    plt.xticks(ticks=np.arange(H.shape[0]), labels=np.arange(1, H.shape[0] + 1))
+    plt.yticks(ticks=np.arange(H.shape[1]), labels=np.arange(1, H.shape[1] + 1))
     plt.show()
 
 def plot_environment(dataset):
@@ -73,8 +81,9 @@ def data_generation(scenario_name,
     return H_abs
 
 H_abs = data_generation('I2_28B',
-                        4, 1, 16, 2,
+                        8, 1, 16, 1,
                         5, 2)
 print(H_abs.shape)
-# plot_heatmat(H_abs)
-
+H_noise = generate_noisy_H(H_abs)
+print(H_noise.shape)
+plot_heatmat(H_noise)
